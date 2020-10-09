3cf7ac26028dadaca1a1d9c911957c52865edfab
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-09 18:01:03 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
   counter = 0 
   while counter < array.length do 
-    puts array[counter].index(value_to_find)
+    puts array[counter] == value_to_find
     counter += 1
   end 
 end
 
-find_element_index([1, 2, 3, 4, 5], 3)
+find_element_index([1, 2, 3], 2) 
