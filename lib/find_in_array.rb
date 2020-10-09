89448a4375c09d7864918b356e04fce29763e0e1
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-09 17:52:31 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   array.length.times do |index| 
     index = array[index].index(value_to_find)
-  return index
+  puts index
 end
 
-puts find_element_index([1, 2, 3], 3)
+find_element_index([1, 2, 3], 3)
